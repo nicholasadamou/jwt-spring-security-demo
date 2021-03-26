92ca6f9066260490fd6395ab2c152dd08d3a1cc5
Nicholas Adamou <nicholasadamouemail@gmail.com>
2021-03-25 20:41:51 -0400

    rm unnecessary throws

--- src/main/java/com/nicholasadamou/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/nicholasadamou/demo/security/config/WebSecurityConfig.java
@@ -33,7 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     @Override
-    public AuthenticationManager authenticationManager() throws Exception {
+    public AuthenticationManager authenticationManager()
+	{
 
         return new ProviderManager(Arrays.asList(authenticationProvider));
     }
